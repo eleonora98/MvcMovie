d256aa56cc7258f43c8f9a23d41490af5e86c1d0
eleonora98 <eleonora_peneva@abv.bg>
2018-11-25 17:15:19 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /* public string Welcome(string name, int ID = 1)
+         {
+            // return "This is the Welcome action method...name= "+name+"ID= "+ID;
+             return HtmlEncoder.Default.Encode($"Hello {name}, ID is: {ID}");
+         }*/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
